c9a0214512704ab938b140773639cb9483b8c882
Nizar <nizarzayto@gmail.com>
2023-12-26 01:41:05 +0100

    Added assets added strings class added colors class added assets class

---
+++ lib/core/app_assets.dart
@@ -0,0 +1,31 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesFogetPassword
+  /// assets/images/fogetPassword.png
+  static const String imagesFogetPassword = "assets/images/fogetPassword.png";
+
+  /// Assets for imagesOnBoarding1
+  /// assets/images/onBoarding1.png
+  static const String imagesOnBoarding1 = "assets/images/onBoarding1.png";
+
+  /// Assets for imagesOnBoarding2
+  /// assets/images/onBoarding2.png
+  static const String imagesOnBoarding2 = "assets/images/onBoarding2.png";
+
+  /// Assets for imagesOnBoarding3
+  /// assets/images/onBoarding3.png
+  static const String imagesOnBoarding3 = "assets/images/onBoarding3.png";
+
+  /// Assets for imagesSuccessOrder
+  /// assets/images/success_order.png
+  static const String imagesSuccessOrder = "assets/images/success_order.png";
+
+  /// Assets for imagesVector1
+  /// assets/images/vector1.svg
+  static const String imagesVector1 = "assets/images/vector1.svg";
+
+  /// Assets for imagesVector2
+  /// assets/images/vector2.svg
+  static const String imagesVector2 = "assets/images/vector2.svg";
+}
---
+++ lib/core/app_router.dart
@@ -0,0 +1,17 @@
+import 'package:dalel/features/splash/presentation/views/splash_view.dart';
+import 'package:go_router/go_router.dart';
+
+abstract class AppRouter {
+  static const kHomeView = '/homeView';
+  static const kBookDetailsView = '/bookDetailsView';
+  static const kSearchView = '/searchView';
+
+  static final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const SplashView(),
+      ),
+    ],
+  );
+}
