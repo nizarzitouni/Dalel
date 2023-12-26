b07ce82458ebbd610ab2b94bb313c84ede42b521
Nizar <nizarzayto@gmail.com>
2023-12-26 01:18:18 +0100

    Folder structors and features

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:dalel/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Dalel());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
